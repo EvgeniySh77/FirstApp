3d9d0b36a161e6be0a9f55bd24ea916a1c640ab3
EvgeniySh77 <evgen.sh77@mail.ru>
2021-12-24 23:30:19 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace FirstApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            string myName;
+            myName = "Evgeniy";
+
+            Console.WriteLine(myName);
+
+            Console.ReadKey();            
+        }
+    }
+}
